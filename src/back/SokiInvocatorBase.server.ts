ee287b61d3c72e50b4d325c4cdbc82d93eee90e5
PSlobodskoy <PSlobodskoy@one.kz>
2025-02-15 10:09:17 +0300

    fix(): tg changes logging on prod modifies only

--- src/back/SokiInvocatorBase.server.ts
+++ src/back/SokiInvocatorBase.server.ts
@@ -1,8 +1,9 @@
 import { InvocatorServerEvent, LocalSokiAuth, SokiVisit } from 'shared/api';
 import { makeSokiInvocatorBase } from 'shared/api/complect/SokiInvocatorBase.master';
-import { smylib } from 'shared/utils';
+import { emptyFunc, smylib } from 'shared/utils';
 import { WebSocket } from 'ws';
 import { onSokiServerEventerInvocatorInvoke } from './complect/soki/eventers';
+import { backConfig } from './config/backConfig';
 import { jesmylChangesBot } from './sides/telegram-bot/control/jesmylChangesBot';
 
 export type SokiServerInvocatorTool = { client: WebSocket; auth: LocalSokiAuth | und; visit: SokiVisit | und };
@@ -12,20 +13,27 @@ export const SokiInvocatorBaseServer = makeSokiInvocatorBase<
   'SokiInvocatorBaseServer',
   SokiServerInvocatorTool,
   string | ((tool: SokiServerInvocatorTool) => string)
->(false, 'SokiInvocatorBaseServer', onSokiServerEventerInvocatorInvoke, (titleScalar, { tool, method, name }) => {
-  if (titleScalar === '') return;
+>(
+  false,
+  'SokiInvocatorBaseServer',
+  onSokiServerEventerInvocatorInvoke,
+  backConfig.isTest
+    ? emptyFunc
+    : (titleScalar, { tool, method, name }) => {
+        if (titleScalar === '') return;
 
-  const title = smylib.isFunc(titleScalar) ? titleScalar(tool) : titleScalar;
+        const title = smylib.isFunc(titleScalar) ? titleScalar(tool) : titleScalar;
 
-  if (title === '') return;
-  const text = `<code>${name}.${method}</code>\n\n<b>${title}</b>`;
+        if (title === '') return;
+        const text = `<code>${name}.${method}</code>\n\n<b>${title}</b>`;
 
-  jesmylChangesBot.postMessage(
-    tool.auth
-      ? `${`${tool.auth.fio} ${tool.auth.nick ? `t.me/${tool.auth.nick}` : ''}\n`}` +
-          text +
-          `\n\n<blockquote expandable>${JSON.stringify(tool.auth, null, 1)}</blockquote>`
-      : text,
-    { parse_mode: 'HTML' },
-  );
-});
+        jesmylChangesBot.postMessage(
+          tool.auth
+            ? `${`${tool.auth.fio} ${tool.auth.nick ? `t.me/${tool.auth.nick}` : ''}\n`}` +
+                text +
+                `\n\n<blockquote expandable>${JSON.stringify(tool.auth, null, 1)}</blockquote>`
+            : text,
+          { parse_mode: 'HTML' },
+        );
+      },
+);
