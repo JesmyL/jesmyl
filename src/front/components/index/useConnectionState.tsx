71b590536010d5a3e6c8eb8003c59bd620909aa4
PSlobodskoy <PSlobodskoy@one.kz>
2025-02-15 08:49:43 +0300

    fix(): no connection-state loader - offline indicator only

--- src/front/components/index/useConnectionState.tsx
+++ src/front/components/index/useConnectionState.tsx
@@ -1,36 +1,20 @@
+import { addEventListenerPipe, hookEffectPipe } from 'front/complect/hookEffectPipe';
 import { useEffect, useState } from 'react';
-import { ThrowEvent } from '../../complect/eventer/ThrowEvent';
-import { TheIconLoading } from '../../complect/the-icon/IconLoading';
 import { IconAlert01StrokeRounded } from '../../complect/the-icon/icons/alert-01';
-import { soki } from '../../soki';
 
 export default function useConnectionState(className?: string) {
-  const [isConnected, setIsConnected] = useState(true);
   const [isOnline, setIsOnline] = useState(true);
-  const [isFocused, setIsFocused] = useState(true);
 
   useEffect(
     () =>
-      hookEffectLine().effect(
-        soki.onConnectionState(setIsConnected),
-        ThrowEvent.listenIsOnline(setIsOnline),
-        ThrowEvent.listenIsWinFocused(setIsFocused),
-      ),
+      hookEffectPipe()
+        .pipe(
+          addEventListenerPipe(window, 'online' as never, () => setIsOnline(true)),
+          addEventListenerPipe(window, 'offline' as never, () => setIsOnline(false)),
+        )
+        .effect(),
     [],
   );
 
-  useEffect(() => {
-    if (isFocused) soki.ping();
-  }, [isFocused]);
-
-  return isOnline ? (
-    isConnected ? null : (
-      <TheIconLoading
-        className={className}
-        onClick={soki.ping}
-      />
-    )
-  ) : (
-    <IconAlert01StrokeRounded className={'color--ko ' + className} />
-  );
+  return isOnline || <IconAlert01StrokeRounded className={'color--ko ' + className} />;
 }
