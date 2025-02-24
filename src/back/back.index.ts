9057d719b7441a84978d2e9f176bf4402f02565e
PSlobodskoy <PSlobodskoy@one.kz>
2025-02-24 08:51:01 +0300

    fix(): removed prisma dbs + usages

--- src/back/back.index.ts
+++ src/back/back.index.ts
@@ -6,7 +6,6 @@ import { scheduleWidgetMessageCatcher } from './apps/index/schedules/tg-bot-info
 import { initTgScheduleInform } from './apps/index/schedules/tg-bot-inform/tg-inform';
 import { sokiServer } from './complect/soki/SokiServer';
 import { baseMessagesCatcher } from './sides/telegram-bot/complect/message-catchers';
-import { invitesTgBotListener } from './sides/telegram-bot/invites/invites.bot';
 
 sokiServer.start();
 
@@ -20,4 +19,3 @@ baseMessagesCatcher.register();
 scheduleWidgetMessageCatcher.register();
 
 startCrTgAlarm();
-invitesTgBotListener();
