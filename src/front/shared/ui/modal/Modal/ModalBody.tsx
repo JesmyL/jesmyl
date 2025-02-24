43c0ed00b98da624fac98cbc0417373a1ab6dad0
PSlobodskoy <PSlobodskoy@one.kz>
2025-02-24 12:49:43 +0300

    fix(ModalBody): has optional className prop

--- src/front/shared/ui/modal/Modal/ModalBody.tsx
+++ src/front/shared/ui/modal/Modal/ModalBody.tsx
@@ -2,8 +2,9 @@ import { StyledModalBody } from '../styled';
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function ModalBody({ children }: Props) {
-  return <StyledModalBody>{children}</StyledModalBody>;
+export function ModalBody({ children, className }: Props) {
+  return <StyledModalBody className={className}>{children}</StyledModalBody>;
 }
