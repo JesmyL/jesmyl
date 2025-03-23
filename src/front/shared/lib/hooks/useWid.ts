d23dd1a6fb35f3b85f77139ee50a95237fbaad98
PSlobodskoy <PSlobodskoy@one.kz>
2025-03-23 21:39:05 +0300

    feat(useWid): add return type

--- src/front/shared/lib/hooks/useWid.ts
+++ src/front/shared/lib/hooks/useWid.ts
@@ -3,5 +3,10 @@ import { makeRegExp } from 'shared/utils';
 
 const deps: [] = [];
 
-export const makeWid = () => ('wid_' + Date.now() + Math.random() + Math.random()).replace(makeRegExp('/\\./g'), '_');
+export const enum UsedWid {
+  def = '',
+}
+
+export const makeWid = () =>
+  ('wid_' + Date.now() + Math.random() + Math.random()).replace(makeRegExp('/\\./g'), '_') as UsedWid;
 export const useWid = () => useMemo(makeWid, deps);
