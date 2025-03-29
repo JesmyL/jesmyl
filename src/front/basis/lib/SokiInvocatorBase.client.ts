f6ea6f7a7923208b5ddab7b247d2441a2b60fcd5
PSlobodskoy <PSlobodskoy@one.kz>
2025-03-29 09:32:14 +0300

    fix(): import fix

--- src/front/basis/lib/SokiInvocatorBase.client.ts
+++ src/front/basis/lib/SokiInvocatorBase.client.ts
@@ -1,6 +1,6 @@
+import { environment } from 'front/environment';
+import { onSokiClientEventerInvocatorInvoke } from 'front/eventers';
 import { makeSokiInvocatorBase } from 'shared/api/complect/SokiInvocatorBase.master';
-import { environment } from '../../environments';
-import { onSokiClientEventerInvocatorInvoke } from '../../eventers';
 
 export const SokiInvocatorBaseClient = makeSokiInvocatorBase(
   environment.isTest,
