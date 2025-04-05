bfede55ee9d38523b3749399879bf2fa1ac4daf6
PSlobodskoy <PSlobodskoy@one.kz>
2025-04-05 14:49:26 +0300

    fix(TheButton): has color

--- src/front/shared/ui/TheButton.tsx
+++ src/front/shared/ui/TheButton.tsx
@@ -1,22 +1,29 @@
 import { HTMLAttributes } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ConfirmContent } from './modal/confirm/ConfirmContent';
+
+const xColors = ['x2', 'x3', 'x5'];
+
+type Props = {
+  color?: (typeof xColors)[number];
+};
 
 export function TheButton({
   disabled,
   confirm,
   onClick,
   ...props
-}: HTMLAttributes<HTMLDivElement> & {
-  disabled?: boolean;
-  confirm?: boolean | string;
-}) {
+}: Props &
+  HTMLAttributes<HTMLDivElement> & {
+    disabled?: boolean;
+    confirm?: boolean | string;
+  }) {
   return (
     <ConfirmContent
       confirm={onClick !== undefined && (confirm === true ? props.children : confirm)}
       content={onConfirm => {
         return (
-          <Button
+          <StyledButton
             {...props}
             className={`the-button pointer ${props.className || ''}${disabled ? ' disabled' : ''}`}
             onClick={async event => {
@@ -29,10 +36,18 @@ export function TheButton({
   );
 }
 
-const Button = styled.div`
+const StyledButton = styled.div<Props>`
   display: inline-block;
   position: relative;
-  border: var(--color--2) 4px solid;
   border-radius: 2em;
   padding: 0.5em 2.2em;
+
+  ${props =>
+    props.color && xColors.includes(props.color)
+      ? css`
+          border: var(--color--${props.color[1]}) 4px solid;
+        `
+      : css`
+          border: var(--color--2) 4px solid;
+        `}
 `;
