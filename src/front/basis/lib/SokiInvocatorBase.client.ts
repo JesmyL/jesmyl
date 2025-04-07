45b8927b97544755bb771f93ae43a94f43442238
PSlobodskoy <PSlobodskoy@one.kz>
2025-04-07 13:48:25 +0300

    fix(): fix

--- src/front/basis/lib/SokiInvocatorBase.client.ts
+++ src/front/basis/lib/SokiInvocatorBase.client.ts
@@ -1,9 +1,11 @@
 import { environment } from 'front/environment';
 import { onSokiClientEventerInvocatorInvoke } from 'front/eventers';
 import { makeSokiInvocatorBase } from 'shared/api/complect/SokiInvocatorBase.master';
+import { emptyFunc } from 'shared/utils';
 
 export const SokiInvocatorBaseClient = makeSokiInvocatorBase({
   isNeedCheckClassName: environment.isTest,
   classNamePostfix: 'SokiInvocatorBaseClient',
   eventerValue: onSokiClientEventerInvocatorInvoke,
+  onErrorMessage: emptyFunc,
 });
