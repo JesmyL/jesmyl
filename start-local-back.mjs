4071f9e6745ae9d737d050bc66ba65bf23fdd91b
PSlobodskoy <PSlobodskoy@one.kz>
2025-04-07 08:32:40 +0300

    fix(start-local-back): can copy actual files to local

--- start-local-back.mjs
+++ start-local-back.mjs
@@ -1,9 +1,19 @@
 import { exec } from 'child_process';
+import fs from 'fs';
 import { buildBackIndexFile } from './build-back-index.mjs';
+import { walkAllFiles } from './utils.mjs';
 
 const errorsToPass = new Set([
   'error: [polling_error] {"code":"ETELEGRAM","message":"ETELEGRAM: 409 Conflict: terminated by other getUpdates request; make sure that only one bot instance is running"}\n',
 ]);
+
+if (!'is need update files') {
+  ['cm', 'index'].forEach(appName => {
+    walkAllFiles(`src/back/apps/${appName}/+case`, (filePath, name) => {
+      fs.writeFileSync(`src/back/file-stores/apps/${appName}/${name}`, '' + fs.readFileSync(filePath));
+    });
+  });
+}
 
 (async () => {
   await buildBackIndexFile();
