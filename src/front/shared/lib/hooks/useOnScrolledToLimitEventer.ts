ea2850146f78bb740bcd5f943790062265cf59c0
PSlobodskoy <PSlobodskoy@one.kz>
2025-04-09 10:03:15 +0300

    fix(useOnScrolledToLimitEventer): delay listens for scroll parent finding

--- src/front/shared/lib/hooks/useOnScrolledToLimitEventer.ts
+++ src/front/shared/lib/hooks/useOnScrolledToLimitEventer.ts
@@ -1,43 +1,51 @@
 import { RefObject, useEffect, useMemo } from 'react';
-import { Eventer } from 'shared/utils';
+import { emptyFunc, Eventer } from 'shared/utils';
 import { addEventListenerPipe, hookEffectPipe } from '../hookEffectPipe';
 
 export const useOnScrolledToLimitEventer = (listRef: RefObject<HTMLDivElement | null>) => {
   const eventerScope = useMemo(() => Eventer.createValue<'start' | 'end'>(), []);
 
   useEffect(() => {
-    if (listRef.current === null) return;
-    let target = listRef.current;
+    let onUnmount = emptyFunc;
+    const timeout = setTimeout(() => {
+      if (listRef.current === null) return;
+      let target = listRef.current;
 
-    while (true) {
-      if (target.scrollHeight !== target.clientHeight) break;
-      if (target.parentElement === null) return;
-      target = target.parentElement as never;
-    }
+      while (true) {
+        if (target.scrollHeight !== target.clientHeight) break;
+        if (target.parentElement === null) return;
+        target = target.parentElement as never;
+      }
 
-    let prevIsTopBorder = false;
-    let prevIsBottomBorder = true;
-    let isTopBorder = false;
-    let isBottomBorder = false;
+      let prevIsTopBorder = false;
+      let prevIsBottomBorder = true;
+      let isTopBorder = false;
+      let isBottomBorder = false;
 
-    return hookEffectPipe()
-      .pipe(
-        addEventListenerPipe(target, 'scroll', () => {
-          const scrollTop = Math.abs(target.scrollTop);
+      onUnmount = hookEffectPipe()
+        .pipe(
+          addEventListenerPipe(target, 'scroll', () => {
+            const scrollTop = Math.abs(target.scrollTop);
 
-          isTopBorder =
-            target.scrollHeight - scrollTop < target.clientHeight * 2 ||
-            target.scrollHeight - scrollTop < target.clientHeight * 0.2;
-          isBottomBorder = scrollTop < target.clientHeight || scrollTop < target.clientHeight * 0.2;
+            isTopBorder =
+              target.scrollHeight - scrollTop < target.clientHeight * 2 ||
+              target.scrollHeight - scrollTop < target.clientHeight * 0.2;
+            isBottomBorder = scrollTop < target.clientHeight || scrollTop < target.clientHeight * 0.2;
 
-          if (!prevIsTopBorder && isTopBorder) eventerScope.invoke('end');
-          if (!prevIsBottomBorder && isBottomBorder) eventerScope.invoke('start');
+            if (!prevIsTopBorder && isTopBorder) eventerScope.invoke('end');
+            if (!prevIsBottomBorder && isBottomBorder) eventerScope.invoke('start');
 
-          prevIsTopBorder = isTopBorder;
-          prevIsBottomBorder = isBottomBorder;
-        }),
-      )
-      .effect();
+            prevIsTopBorder = isTopBorder;
+            prevIsBottomBorder = isBottomBorder;
+          }),
+        )
+        .effect();
+    }, 100);
+
+    return () => {
+      onUnmount();
+      clearTimeout(timeout);
+    };
   }, [eventerScope, listRef]);
 
   return eventerScope;
