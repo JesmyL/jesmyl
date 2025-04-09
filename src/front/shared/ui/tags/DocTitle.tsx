aa446dfba015ddd580e278789c73d847332e71e2
PSlobodskoy <PSlobodskoy@one.kz>
2025-04-09 10:01:48 +0300

    fix(DocTitle): title can be nil

--- src/front/shared/ui/tags/DocTitle.tsx
+++ src/front/shared/ui/tags/DocTitle.tsx
@@ -1,16 +1,17 @@
-import { memo, useEffect } from 'react';
+import { useEffect } from 'react';
 
 const initTitle = document.title;
 const ret = () => {
   document.title = initTitle;
 };
 
-export const DocTitle = memo(function DocTitle({ title }: { title: string }) {
+export const DocTitle = ({ title }: { title: string | nil }) => {
   useEffect(() => {
+    if (title == null) return ret();
     document.title = `${title} × ${initTitle}`;
 
     return ret;
   }, [title]);
 
   return <></>;
-});
+};
