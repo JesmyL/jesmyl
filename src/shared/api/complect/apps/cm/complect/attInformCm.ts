65f7602c01dabed17d879c0b2f33a25cb39c1051
PSlobodskoi <PSlobodskoi@fortebank.com>
2025-08-21 10:42:49 +0500

    fix(): schedule com attachment link fix

--- src/shared/api/complect/apps/cm/complect/attInformCm.ts
+++ src/shared/api/complect/apps/cm/complect/attInformCm.ts
@@ -4,10 +4,10 @@ export const makeCmScheduleWidgetComListUrl = (
   schw: number | string,
   dayi: number | string,
   eventMi: number | string,
-) => `${hosts.host}/!other/cm/schs?attKey=%5Bcm%5D%3A&schw=${schw}&dayi=${dayi}&eventMi=${eventMi}`;
+) => `${hosts.host}/!other/cm/schs?attKey=%5Bcm%5D%3Acoms&schw=${schw}&dayi=${dayi}&eventMi=${eventMi}`;
 
 export const cmTgAttInform: AttTgInformStorage = {
-  '[cm]:coms': (_value: unknown, eventTitle, schedule, dayi, event) => {
+  '[cm]:coms': (_value, eventTitle, schedule, dayi, event) => {
     return `Список песен${eventTitle}:\n${makeCmScheduleWidgetComListUrl(schedule.w, dayi, event.mi)}\n\n`;
   },
 };
