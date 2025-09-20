bfad22792a29911f7dace8a37150329c418635ad
PSlobodskoi <PSlobodskoi@fortebank.com>
2025-09-20 22:30:19 +0500

    fix(): some style + textarea adaptive height

--- src/front/shared/components/ui/textarea.tsx
+++ src/front/shared/components/ui/textarea.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { mylib } from '#shared/lib/my-lib';
 import { cn } from '#shared/lib/utils';
 
 function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
@@ -11,13 +12,21 @@ function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
         'border-ring ring-ring/50 focus-visible:ring-ring/80 ring-[3px]',
         'aria-invalid:ring-destructive/20 max-h-[15lh]',
         'dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-        'dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md',
+        'dark:bg-input/30 flex min-h-16 w-full rounded-md',
         'border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow]',
         'outline-none disabled:cursor-not-allowed',
         'disabled:opacity-50 md:text-sm',
         className,
       )}
       {...props}
+      onFocus={event => {
+        props.onFocus?.(event);
+        mylib.setInputHeightByContent(event.currentTarget);
+      }}
+      onInput={event => {
+        props.onInput?.(event);
+        mylib.setInputHeightByContent(event.currentTarget);
+      }}
     />
   );
 }
