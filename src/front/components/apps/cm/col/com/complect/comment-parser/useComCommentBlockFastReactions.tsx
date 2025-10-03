946a8ac3649c6513dd8b5cc7a369293787a7520a
PSlobodskoi <PSlobodskoi@fortebank.com>
2025-10-03 09:15:03 +0500

    fix(cm): open comment modal trigger is touch and mouse actions

--- src/front/components/apps/cm/col/com/complect/comment-parser/useComCommentBlockFastReactions.tsx
+++ src/front/components/apps/cm/col/com/complect/comment-parser/useComCommentBlockFastReactions.tsx
@@ -5,6 +5,7 @@ import { cmIsComMiniAnchorAtom } from '$cm/atoms';
 import { cmComCommentRedactOrdSelectorIdAtom } from '$cm/basis/lib/store/atoms';
 import { useAtomValue } from 'atomaric';
 import { useEffect } from 'react';
+import { wait } from 'shared/utils';
 import { Com } from '../../Com';
 import { ComBlockCommentMakerCleans } from './Cleans';
 
@@ -14,36 +15,43 @@ export const useComCommentBlockFastReactions = (listRef: React.RefObject<HTMLDiv
   useEffect(() => {
     if (isMiniAnchor) return;
     let isFirstClick = true;
+    let firstEvent = '';
+
+    const onDown = async (event: MouseEvent & TouchEvent) => {
+      if (!firstEvent) firstEvent = event.type;
+      if (firstEvent !== event.type) return;
+
+      if (isFirstClick) {
+        isFirstClick = false;
+        setTimeout(() => {
+          isFirstClick = true;
+          firstEvent = '';
+        }, 500);
+        return;
+      }
+
+      const comOrders = com.orders;
+      if (comOrders === null) return;
+      const visibleOrders = comOrders.filter(ComBlockCommentMakerCleans.withHeaderTextOrderFilter);
+
+      const { node, foundClassNames } = getParentNodeWithClassName(event, 'styled-block', ['styled-header']);
+
+      if (node === null || !foundClassNames['styled-header']) return;
+
+      const orderNN = +node.getAttribute('visible-ord-nn')!;
+      if (mylib.isNaN(orderNN)) return;
+      const ord = visibleOrders[orderNN - 1];
+      if (ord == null) return;
+
+      await wait(500);
+
+      cmComCommentRedactOrdSelectorIdAtom.set(ComBlockCommentMakerCleans.makeOrdSelector(ord));
+    };
 
     return hookEffectPipe()
       .pipe(
-        addEventListenerWithDelayPipe(
-          700,
-          () => listRef.current,
-          'mousedown',
-          async event => {
-            const comOrders = com.orders;
-            if (comOrders === null) return;
-            const visibleOrders = comOrders.filter(ComBlockCommentMakerCleans.withHeaderTextOrderFilter);
-
-            if (isFirstClick) {
-              isFirstClick = false;
-              setTimeout(() => (isFirstClick = true), 500);
-              return;
-            }
-
-            const { node, foundClassNames } = getParentNodeWithClassName(event, 'styled-block', ['styled-header']);
-
-            if (node === null || !foundClassNames['styled-header']) return;
-
-            const orderNN = +node.getAttribute('visible-ord-nn')!;
-            if (mylib.isNaN(orderNN)) return;
-            const ord = visibleOrders[orderNN - 1];
-            if (ord == null) return;
-
-            cmComCommentRedactOrdSelectorIdAtom.set(ComBlockCommentMakerCleans.makeOrdSelector(ord));
-          },
-        ),
+        addEventListenerWithDelayPipe(1000, () => listRef.current, 'mousedown', onDown),
+        addEventListenerWithDelayPipe(1000, () => listRef.current, 'touchstart', onDown),
       )
       .effect();
   }, [com.orders, isMiniAnchor, listRef]);
