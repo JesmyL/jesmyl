2011d2d07a7ecbc5a3c7f1f5a96cd444ee904590
PSlobodskoi <PSlobodskoi@fortebank.com>
2025-10-06 10:39:20 +0500

    feat(): CRUD checker rule and operation can be Array types

--- src/shared/utils/index/utils.ts
+++ src/shared/utils/index/utils.ts
@@ -1,3 +1,4 @@
+import { mylib } from '#shared/lib/my-lib';
 import { IndexAppAccessRightTitles } from 'shared/model/index/access-rights';
 import { SMyLib } from '../SMyLib';
 
@@ -8,6 +9,7 @@ const operations = {
   D: 3,
 };
 
+export type CRUDOperation = keyof typeof operations;
 export const accessRightsCRUDOperations = SMyLib.keys(operations);
 
 export const checkUserScopeAccessRight = <
@@ -17,16 +19,32 @@ export const checkUserScopeAccessRight = <
 >(
   rights: Rights,
   scope: Scope,
-  rule: Rule,
-  operation: keyof typeof operations = 'R',
+  rule: Rule | Rule[],
+  operation: CRUDOperation | CRUDOperation[] = 'R',
 ) => {
-  const accessNumber = rights?.[scope]?.[rule] ?? 0;
-  const accessBinary = (accessNumber >>> 0).toString(2).padStart(4, '0');
+  try {
+    if (rights[scope] == null) return false;
 
-  return accessBinary[operations[operation]] === '1';
+    rule = mylib.isArr(rule) ? rule : [rule];
+    operation = mylib.isArr(operation) ? operation : [operation];
+
+    if (operation.length === 0 || rule.length === 0) return false;
+
+    for (const ruleItem of rule) {
+      const accessBinary = (rights[scope][ruleItem]! >>> 0).toString(2).padStart(4, '0');
+
+      for (const operationItem of operation) {
+        if (accessBinary[operations[operationItem]] !== '1') return false;
+      }
+    }
+
+    return true;
+  } catch (_) {
+    return false;
+  }
 };
 
-export const updateCRUDAccesRightValue = (prev: number, operation: keyof typeof operations, value: boolean) => {
+export const updateCRUDAccesRightValue = (prev: number, operation: CRUDOperation, value: boolean) => {
   const accessArr = (prev >>> 0).toString(2).padStart(4, '0').split('');
   accessArr[operations[operation]] = value ? '1' : '0';
   return parseInt(accessArr.join(''), 2);
