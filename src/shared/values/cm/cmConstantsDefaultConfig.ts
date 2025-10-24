3ba1b0622d9e999daf96237465a865d91a5fb11e
PSlobodskoi <PSlobodskoi@fortebank.com>
2025-10-24 10:40:13 +0500

    fix(): maxAvailableComLineLength default fix

--- src/shared/values/cm/cmConstantsDefaultConfig.ts
+++ src/shared/values/cm/cmConstantsDefaultConfig.ts
@@ -2,7 +2,7 @@ import { CmConstantsConfig } from 'shared/api';
 
 export const cmConstantsDefaultConfig: CmConstantsConfig = {
   maxFavouritesCount: 30,
-  maxAvailableComLineLength: 37,
+  maxAvailableComLineLength: 47,
   maxSelectedComsCount: 50,
   maxLaterComsVizitedCount: 4,
   maxComCommentAlternativesCount: 3,
