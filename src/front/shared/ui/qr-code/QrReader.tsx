28940d5e752976cd0f88516c55d91a2e92d6e3e4
PSlobodskoi <PSlobodskoi@fortebank.com>
2025-10-29 10:21:03 +0500

    fix(QrReader): sm style fix and props additions

--- src/front/shared/ui/qr-code/QrReader.tsx
+++ src/front/shared/ui/qr-code/QrReader.tsx
@@ -2,27 +2,28 @@ import { IDetectedBarcode, Scanner } from '@yudiel/react-qr-scanner';
 import { Atom } from 'atomaric';
 import { FullContent } from '../fullscreen-content/FullContent';
 
-interface Props {
+type Props = OmitOwn<Parameters<typeof Scanner>[0], 'onScan'> & {
   facingMode?: 'user' | 'environment';
   onReadData: (firstText: string, resultx: IDetectedBarcode[]) => void;
   openAtom: Atom<boolean>;
-}
+};
 
-export const QrReader = ({ facingMode = 'environment', onReadData, openAtom }: Props) => {
+export const QrReader = ({ facingMode = 'environment', onReadData, openAtom, ...props }: Props) => {
   return (
     <FullContent
       openAtom={openAtom}
-      className=" "
+      containerClassName="p-0"
       closable
     >
       <div className="bg-x1 flex center full-size">
         <Scanner
-          constraints={{ facingMode }}
           sound={false}
           onScan={result => {
             if (result?.[0] == null) return;
             onReadData(result[0].rawValue, result);
           }}
+          {...props}
+          constraints={{ ...props.constraints, facingMode }}
         />
       </div>
     </FullContent>
