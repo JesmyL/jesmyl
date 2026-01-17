d50adf3e8d7e53fb4a62d64fb30d990cad988438
gfirf <7jesmyl@gmail.com>
2026-01-17 20:36:34 +0300

    feat(cm): add categories by intensity

--- src/front/apps/cm+editor/03-pages/CategoryPage/config/index.ts
+++ src/front/apps/cm+editor/03-pages/CategoryPage/config/index.ts
@@ -1,4 +1,5 @@
 import { CmCat, CmCatTracker, CmCom } from '$cm/ext';
+import { CmComIntensityLevel } from 'shared/api';
 
 export const cmEditorCategoryTrackers: CmCatTracker[] = [
   { title: 'Полный', id: 'full', select: () => true },
@@ -6,4 +7,13 @@ export const cmEditorCategoryTrackers: CmCatTracker[] = [
   { title: 'Список', id: 'list', select: (com: CmCom, cat: CmCat) => cat.stack?.includes(com.wid) },
   { title: 'Язык - Русский', id: 'lang:ru', select: (com: CmCom) => !com.langi },
   { title: 'Язык - Украинский', id: 'lang:ua', select: (com: CmCom) => com.langi === 1 },
+  { title: 'Очень медленные', id: 'int:1', select: (com: CmCom) => com.top.d === CmComIntensityLevel.TooSlow },
+  { title: 'Медленные', id: 'int:2', select: (com: CmCom) => com.top.d === CmComIntensityLevel.Slow },
+  {
+    title: 'Средней интенсивности',
+    id: 'int:3',
+    select: (com: CmCom) => com.top.d == null || com.top.d === CmComIntensityLevel.Medium,
+  },
+  { title: 'Быстрые', id: 'int:4', select: (com: CmCom) => com.top.d === CmComIntensityLevel.Fast },
+  { title: 'Очень быстрые', id: 'int:5', select: (com: CmCom) => com.top.d === CmComIntensityLevel.TooFast },
 ];
