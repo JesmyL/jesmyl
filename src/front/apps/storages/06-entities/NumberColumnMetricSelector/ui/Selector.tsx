f7344f9201de310dd8c2fc2231e76c64fcdf72c0
gfirf <7jesmyl@gmail.com>
2026-02-03 08:51:37 +0300

    feat(storages): can select custom metric name for numbers

--- src/front/apps/storages/06-entities/NumberColumnMetricSelector/ui/Selector.tsx
+++ src/front/apps/storages/06-entities/NumberColumnMetricSelector/ui/Selector.tsx
@@ -1,25 +1,75 @@
+import { Button } from '#shared/components';
+import { mylib } from '#shared/lib/my-lib';
 import { Dropdown } from '#shared/ui/dropdown/Dropdown';
+import { usePrompt } from '#shared/ui/modal';
 import { StoragesColumnEditTypeProps } from '$storages/shared/model/col-edit';
 import { storagesTsjrpcClient } from '$storages/shared/tsjrpc/basic.tsjrpc.methods';
 import { StoragesColumnType } from 'shared/model/storages/rack.model';
 
-const metrics = ['₽', '$', '€', '£', 'шт', 'м', 'мин'];
-
 export const StoragesNumberColumnMetricSelector = (props: StoragesColumnEditTypeProps<StoragesColumnType.Number>) => {
+  const prompt = usePrompt();
+  const maxCustomMetricLen = 7;
+  const value = props.column.mt;
+
+  const onSend = (mt: string) =>
+    storagesTsjrpcClient.editColumnFields({
+      coli: props.coli,
+      data: { [StoragesColumnType.Number]: { mt } },
+      rackw: props.rack.w,
+      ...props.nestedSelectors,
+    });
+
+  const metrics = {
+    '₽': 0,
+    $: 0,
+
+    шт: 0,
+
+    м: 0,
+    см: 0,
+    км: 0,
+
+    мин: 0,
+    час: 0,
+    сек: 0,
+
+    лит: 0,
+    мл: 0,
+    куб: 0,
+
+    кг: 0,
+    г: 0,
+    мг: 0,
+
+    '€': 0,
+    '£': 0,
+  };
+
   return (
     <Dropdown
-      id={props.column.mt}
+      id={value}
       nullTitle="-"
       hiddenArrow
-      items={metrics.map(id => ({ id, title: id }))}
-      onSelectId={id =>
-        storagesTsjrpcClient.editColumnFields({
-          coli: props.coli,
-          data: { [StoragesColumnType.Number]: { mt: id } },
-          rackw: props.rack.w,
-          ...props.nestedSelectors,
-        })
+      items={mylib
+        .keys(metrics)
+        .map(id => ({ id, title: id }) as { id: string; title: React.ReactNode })
+        .concat(
+          value && !(value in metrics) ? { id: value, title: <div className="text-x7 opacity-50">{value}</div> } : [],
+        )}
+      addContent={
+        <Button
+          onClick={async () => {
+            const customMeter = await prompt(`Введи название измерения (максимум символов - ${maxCustomMetricLen})`);
+
+            if (!customMeter) return;
+
+            return onSend(customMeter.slice(0, maxCustomMetricLen));
+          }}
+        >
+          Другое
+        </Button>
       }
+      onSelectId={onSend}
     />
   );
 };
