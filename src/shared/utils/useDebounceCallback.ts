11361cf04fcb6776bab3630399ed88181afebcc7
gfirf <7jesmyl@gmail.com>
2026-02-15 16:47:15 +0300

    fix(): useDebounceCallback remove unnecessary

--- src/shared/utils/useDebounceCallback.ts
+++ src/shared/utils/useDebounceCallback.ts
@@ -1,16 +1,12 @@
 import { useCallback, useRef } from 'react';
 
 export const useDebounceCallback = (debounceTime = 500) => {
-  const debounceRef = useRef<TimeOut | null>(null);
+  const debounceRef = useRef<TimeOut>(null);
 
   return useCallback(
     (callback: () => void, subDebounceTime = debounceTime) => {
       clearTimeout(debounceRef.current!);
-
-      debounceRef.current = setTimeout(() => {
-        callback();
-        debounceRef.current = null;
-      }, subDebounceTime);
+      debounceRef.current = setTimeout(callback, subDebounceTime);
     },
     [debounceTime],
   );
