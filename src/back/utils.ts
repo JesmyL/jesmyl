72d60d48273b89959d701691d084562e3087e8b9
gfirf <7jesmyl@gmail.com>
2026-05-18 15:56:59 +0300

    fix(): small opt

--- src/back/utils.ts
+++ src/back/utils.ts
@@ -1,23 +1,18 @@
 import { LocalSokiAuth, SokiAuthLogin, SokiVisit } from 'shared/api';
 import { appVersionFileStore } from './apps/index/file-stores';
 
-export const userVisitStringified = (visitInfo: SokiVisit | nil) => {
-  if (visitInfo == null) return '';
-  return (
-    `${visitInfo.urls[0]}\n\n<code>${JSON.stringify(
-      { ...visitInfo, version: `${visitInfo.version}/${appVersionFileStore.getValue().num}` },
-      null,
-      1,
-    )}\n` + `Разница: ${Date.now() - visitInfo.clientTm}мс</code>`
-  );
-};
+export const userVisitStringified = (visitInfo: SokiVisit | nil) =>
+  visitInfo
+    ? `${visitInfo.urls[0]}\n\n<code>${JSON.stringify(
+        { ...visitInfo, version: `${visitInfo.version}/${appVersionFileStore.getValue().num}` },
+        null,
+        1,
+      )}\nРазница: ${Date.now() - visitInfo.clientTm}мс</code>`
+    : '';
 
-export const userAuthStringified = (auth: LocalSokiAuth | nil) => {
-  return (
-    `<div style='white-space:pre-line'>${auth ? `${auth.fio}${auth.nick && auth.tgId ? ` https://t.me/${auth.nick}` : ''}` : 'Неизвестный'}\n\n` +
-    `<code>${auth ? JSON.stringify(auth, null, 1) : ''}</code></div>`
-  );
-};
+export const userAuthStringified = (auth: LocalSokiAuth | nil) =>
+  `<div style='white-space:pre-line'>${auth ? `${auth.fio}${auth.nick && auth.tgId ? ` https://t.me/${auth.nick}` : ''}` : 'Неизвестный'}\n\n` +
+  `<code>${auth ? JSON.stringify(auth, null, 1) : ''}</code></div>`;
 
 export const takeLogginedAuthOrThrow = (auth: LocalSokiAuth | nil): LocalSokiAuth & { login: SokiAuthLogin } => {
   if (!auth?.login) throw 'Не авторизован';
