21feca7487367df4988f7c705bf6baa3bfb9732c
gfirf <7jesmyl@gmail.com>
2026-05-19 09:31:41 +0300

    fix(): makeDateLabel() fix

--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,4 +1,5 @@
 import { makeRegExp } from 'regexpert';
+import { smylib } from './SMyLib';
 
 export const convertMd2HTMLMaker = (isForTg: boolean) => {
   const headersReplacer = isForTg ? '<b>$2</b>' : baseHeadersReplacer;
@@ -171,20 +172,32 @@ export const makeDateLabel = (inputDate: number | Date | string) => {
   const startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime();
   const startOfTarget = new Date(date.getFullYear(), date.getMonth(), date.getDate()).getTime();
 
-  const diffInDays = Math.round((startOfTarget - startOfToday) / 86400000);
+  const diffInDays = Math.round((startOfTarget - startOfToday) / smylib.howMs.inDay);
 
   if (Math.abs(diffInDays) <= 3) {
     const rtf = new Intl.RelativeTimeFormat('ru', { numeric: 'auto' });
     const relative = rtf.format(diffInDays, 'day');
 
-    return relative[0].toUpperCase() + relative.slice(1);
+    return (
+      relative[0].toUpperCase() +
+      relative.slice(1) +
+      date
+        .toLocaleDateString('ru', {
+          minute: '2-digit',
+          hour: '2-digit',
+          day: 'numeric',
+          month: 'long',
+        })
+        .slice(-8)
+    );
   }
 
   return date.toLocaleDateString('ru', {
+    minute: '2-digit',
+    hour: '2-digit',
+    weekday: 'short',
     day: 'numeric',
     month: 'long',
     year: diffInDays < -365 ? 'numeric' : undefined,
-    hour: '2-digit',
-    minute: '2-digit',
   });
 };
