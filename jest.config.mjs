7de63f23039b94842922222e77e3d7c1ba4769d9
gfirf <7jesmyl@gmail.com>
2026-06-17 08:04:27 +0300

    fix(): remove unnecessary code

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,13 +3,6 @@ import { pathsToModuleNameMapper } from 'ts-jest';
 
 const tsConfig = JSON.parse(fs.readFileSync('./tsconfig.json'));
 
-// const namesDict = {};
-// Object.entries(tsConfig.compilerOptions.paths).forEach(([key, value]) => {
-//   namesDict[`^${key.slice(0, -1)}(.*)$`] = `<rootDir>${value.slice(1, -1)}$1`;
-// });
-
-// console.log(namesDict);
-
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
