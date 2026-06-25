a247f1b4d98e623ffbdc7c5fb3ad3d96410a2521
gfirf <7jesmyl@gmail.com>
2026-06-25 07:28:58 +0300

    fix(): WithAtomWithValue usage

--- src/front/shared/ui/CopyTextButton.tsx
+++ src/front/shared/ui/CopyTextButton.tsx
@@ -1,10 +1,10 @@
-import { Atom, atom, useAtomValue } from 'atomaric';
 import { ReactNode } from 'react';
 import { checkIsString } from 'shared/utils/checkIs';
 import { toast } from 'sonner';
 import { twMerge } from 'tailwind-merge';
 import { Modal, ModalBody, ModalHeader } from './modal';
 import { TheIconButton } from './the-icon/TheIconButton';
+import { WithAtomWithValue } from './WithAtomWithValue';
 
 interface Props {
   text: string | (() => string | nil);
@@ -16,44 +16,42 @@ interface Props {
   onClose?: () => void;
 }
 
-let textToCopyAtom: Atom<string>;
-
 export function CopyTextButton({ text, disabled, description, className, message, withoutIcon, onClose }: Props) {
-  textToCopyAtom ??= atom('');
-
-  const textToCopy = useAtomValue(textToCopyAtom);
-
   return (
-    <>
-      <span
-        className={twMerge('flex gap-2 pointer', className)}
-        onClick={event => {
-          event.stopPropagation();
-          const textToWrite = checkIsString(text) ? text : text();
-          if (textToWrite == null) return;
+    <WithAtomWithValue init="">
+      {(textToCopy, textToCopyAtom) => (
+        <>
+          <span
+            className={twMerge('flex gap-2 pointer', className)}
+            onClick={event => {
+              event.stopPropagation();
+              const textToWrite = checkIsString(text) ? text : text();
+              if (textToWrite == null) return;
 
-          try {
-            navigator.clipboard.writeText(textToWrite);
-            toast(message ?? 'Текст скопирован');
-            onClose?.();
-          } catch (_e) {
-            textToCopyAtom.set(textToWrite);
-          }
-        }}
-      >
-        {description}
-        {withoutIcon || (
-          <TheIconButton
-            icon="Copy01"
-            disabled={disabled}
-          />
-        )}
-      </span>
+              try {
+                navigator.clipboard.writeText(textToWrite);
+                toast(message ?? 'Текст скопирован');
+                onClose?.();
+              } catch (_e) {
+                textToCopyAtom.set(textToWrite);
+              }
+            }}
+          >
+            {description}
+            {withoutIcon || (
+              <TheIconButton
+                icon="Copy01"
+                disabled={disabled}
+              />
+            )}
+          </span>
 
-      <Modal openAtom={textToCopyAtom}>
-        <ModalHeader>Не удалось скопировать текст:</ModalHeader>
-        <ModalBody className="user-select-all">{textToCopy}</ModalBody>
-      </Modal>
-    </>
+          <Modal openAtom={textToCopyAtom}>
+            <ModalHeader>Не удалось скопировать текст:</ModalHeader>
+            <ModalBody className="user-select-all">{textToCopy}</ModalBody>
+          </Modal>
+        </>
+      )}
+    </WithAtomWithValue>
   );
 }
