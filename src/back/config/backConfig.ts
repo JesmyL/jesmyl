b6af33367bd97efc9fcd8fdd12f97fc7f6cdc86b
gfirf <7jesmyl@gmail.com>
2026-07-07 08:59:25 +0300

    fix(): sm fx

--- src/back/config/backConfig.ts
+++ src/back/config/backConfig.ts
@@ -5,7 +5,7 @@ import { backConfigTests } from './backConfig.tests';
 export let backConfig = backConfigProd;
 
 try {
-  if ('' + fs.readFileSync(`${__dirname}/.env.mode`) === 'TEST') backConfig = backConfigTests;
+  if (fs.readFileSync(`${__dirname}/.env.mode`, 'utf-8') === 'TEST') backConfig = backConfigTests;
 } catch (_error) {
   //
 }
