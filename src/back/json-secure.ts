943ddae587bb898f30ec07618864e18adb74e9ef
gfirf <7jesmyl@gmail.com>
2026-07-10 11:24:58 +0300

    fix(): json secure opt

--- src/back/json-secure.ts
+++ src/back/json-secure.ts
@@ -11,16 +11,19 @@ export type JsonSecureString<T> = string & { readonly [secureBrand]: T };
 
 export const jsonStringifySecure = <Val>(val: Val, password = lazyEnvJson().SECURE_KEY) => {
   const json = JSON.stringify(val);
-
   const key = crypto.createHash(hashAlg).update(password).digest();
-  const cipher = crypto.createCipheriv(algorithm, key, key.subarray(0, 16));
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const encrypted = cipher.update(json, valEnc, strEnc) + cipher.final(strEnc);
 
-  return (cipher.update(json, valEnc, strEnc) + cipher.final(strEnc)) as JsonSecureString<Val>;
+  return `${iv.toString(strEnc)}:${encrypted}` as JsonSecureString<Val>;
 };
 
-export const jsonParseSecure = <T>(hex: JsonSecureString<T>, password = lazyEnvJson().SECURE_KEY) => {
+export const jsonParseSecure = <T>(secureString: JsonSecureString<T>, password = lazyEnvJson().SECURE_KEY) => {
   const key = crypto.createHash(hashAlg).update(password).digest();
-  const decipher = crypto.createDecipheriv(algorithm, key, key.subarray(0, 16));
+  const [ivHex, encrypted] = secureString.split(':', 2);
+  const buffer = encrypted ? Buffer.from(ivHex, strEnc) : key.subarray(0, 16);
+  const decipher = crypto.createDecipheriv(algorithm, key, buffer);
 
-  return JSON.parse(decipher.update(hex, strEnc, valEnc) + decipher.final(valEnc)) as T;
+  return JSON.parse(decipher.update(encrypted || secureString, strEnc, valEnc) + decipher.final(valEnc)) as T;
 };
