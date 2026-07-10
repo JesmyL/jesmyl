f38964e892a5a42ae3b7efda7ca5f1eb4d38d6ea
gfirf <7jesmyl@gmail.com>
2026-07-10 11:29:15 +0300

    fix(): uncomment updateAllStarts inner

--- src/back/updateAllStarts.ts
+++ src/back/updateAllStarts.ts
@@ -1,30 +1,32 @@
+import { randomBytes } from 'crypto';
+import { Do } from 'shared/enums';
+import { stameskaIconPack } from 'stameska-icon/pack';
+import { indexStameskaIconsFileStore, valuesFileStore } from './apps/index/file-stores';
+import { tokenSecretFileStore } from './complect/soki/file-stores';
+import { emailerConfigFileStorage } from './sides/emailer/file-stores';
+import { tgBotConfig } from './sides/telegram-bot/file-stores';
+
 export const updateAllStarts = () => {
+  if (!Do.It)
+    if (indexStameskaIconsFileStore.getValue() == null) indexStameskaIconsFileStore.setValue(stameskaIconPack);
+
+  if (!Do.It)
+    valuesFileStore.setValue(prev => ({
+      ...prev,
+      // chatUrl: '',
+      // iconSearchLink: '',
+      // desktopLinuxDownLink: `${hostConfig.url}/down/JESMYL_PRO.AppImage`,
+      // desktopWindowsDownLink: `${hostConfig.url}/down/JESMYL_PRO.exe`,
+    }));
+
+  if (!Do.It) tokenSecretFileStore.setValue({ token: randomBytes(60).toString('hex') });
+
+  // generate password in https://id.yandex.ru/security/app-passwords
   // Некоторые хостинги блокируют 465 порт! Нужно разблочить!!!
-  //
-  //
-  //
-  // if (indexStameskaIconsFileStore.getValue() == null) indexStameskaIconsFileStore.setValue(stameskaIconPack);
-  //
-  // valuesFileStore.setValue(prev => ({
-  //   ...prev,
-  //   // chatUrl: '',
-  //   // iconSearchLink: '',
-  //   // desktopLinuxDownLink: `${hostConfig.url}/down/JESMYL_PRO.AppImage`,
-  //   // desktopWindowsDownLink: `${hostConfig.url}/down/JESMYL_PRO.exe`,
-  // }));
-  //
-  //
-  //
-  // tokenSecretFileStore.setValue({ token: randomBytes(60).toString('hex') });
-  //
-  //
-  //
-  // // generate password in https://id.yandex.ru/security/app-passwords
-  // emailerConfigFileStorage.setValue(prev => ({
-  //   ...prev,
-  //   ...
-  // }));
-  //
-  //
-  // tgBotConfig.setValue({ token: '' });
+  if (!Do.It)
+    emailerConfigFileStorage.setValue(prev => ({
+      ...prev,
+    }));
+
+  if (!Do.It) tgBotConfig.setValue({ token: '' });
 };
