0777b6809649b9f22cf31c4ff89caae9a1d4fea2
gfirf <7jesmyl@gmail.com>
2026-07-14 10:51:53 +0300

    fix(): remove constantsConfigFileStore

--- src/back/apps/index/schedules/file-stores.ts
+++ src/back/apps/index/schedules/file-stores.ts
@@ -1,17 +1,9 @@
-import { DirStorage } from 'back/complect/DirStorage';
 import { FileStore } from 'back/complect/FileStore';
-import { ConstantsConfig, IScheduleWidget, IScheduleWidgetWid } from 'shared/api';
+import { ConstantsConfig } from 'shared/api';
 import { constantsDefaultConfig } from 'shared/const/cm/constants.def';
-import { takeDefaultScheduleWidget } from 'shared/const/schedule-widget/const';
 import { iife } from 'shared/utils';
 import { checkIsUndefined } from 'shared/utils/checkIs';
 import { objectKeys } from 'shared/utils/object.utils';
-
-export const schedulesDirStore = new DirStorage<IScheduleWidget, IScheduleWidgetWid, 'w'>({
-  dirPath: '/apps/index/schedules/',
-  idKey: 'w',
-  makeNewItem: takeDefaultScheduleWidget,
-});
 
 export const constantsConfigFileStore = new FileStore<ConstantsConfig>(
   '/apps/index/constantsConfig.json',
