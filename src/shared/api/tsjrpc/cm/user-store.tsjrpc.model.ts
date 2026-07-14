e03c53c6e40d09610b0b863181eed60a8294830d
gfirf <7jesmyl@gmail.com>
2026-07-14 10:19:34 +0300

    fix(cm): add TODO comment

--- src/shared/api/tsjrpc/cm/user-store.tsjrpc.model.ts
+++ src/shared/api/tsjrpc/cm/user-store.tsjrpc.model.ts
@@ -1,6 +1,7 @@
 import { CmComWid, MigratableComToolName } from 'shared/api/complect/apps';
 
 export type CmUserStoreTsjrpcModel = {
+  /** TODO: make send after offline */
   comFav: (args: { comw: CmComWid; is: boolean }) => void;
   favTools: (args: { tools: MigratableComToolName[] }) => void;
 };
