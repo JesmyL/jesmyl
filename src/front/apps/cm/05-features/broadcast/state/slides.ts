209f6419161b5e4db5241df2136fa26e6caf9d61
gfirf <7jesmyl@gmail.com>
2026-07-20 12:27:22 +0300

    fix(cm): useCmBroadcastSlidesContext is not deprecated

--- src/front/apps/cm/05-features/broadcast/state/slides.ts
+++ src/front/apps/cm/05-features/broadcast/state/slides.ts
@@ -2,7 +2,6 @@ import { contextCreator } from '#shared/lib/contextCreator';
 import { emptyFunc } from 'shared/utils';
 import { CmBroadcastSlidesContextState } from '../model/slides';
 
-/** @deprecated divide current slide / slides */
 export const [CmBroadcastInnerSlidesContext, useCmBroadcastSlidesContext] =
   contextCreator<CmBroadcastSlidesContextState>({
     slidei: -2,
