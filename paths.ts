19c1e53758b8d4325b17517c8a3be47d60a754eb
gfirf <7jesmyl@gmail.com>
2026-07-22 18:09:45 +0300

    fix

--- paths.ts
+++ paths.ts
@@ -1,14 +1,12 @@
-import { hostConfig, hostConfigFileName } from './freshHostConfig';
+import { hostConfigFileName } from './freshHostConfig';
 import { deployPathsBasicDict } from './paths.basic';
 
 export const deployPathsDict: Record<string, string[]> = {
   '': [hostConfigFileName, 'hostConfig.mjs'],
+  ...deployPathsBasicDict,
 
-  ...(hostConfig.isUpdateAllStarts
-    ? {
-        ...deployPathsBasicDict,
-        'apps/bible': ['./src/bibles/*.json'],
-        'apps/index': ['~/#/*.json', '~/#/*.secure', '~/#/emailTextingLetterVariants'],
-      }
-    : deployPathsBasicDict),
+  ...{
+    // 'apps/bible': ['./src/bibles/*.json'],
+    // 'apps/index': ['~/#/*.json', '~/#/*.secure', '~/#/emailTextingLetterVariants'],
+  },
 };
--- start-local-back.ts
+++ start-local-back.ts
@@ -5,27 +5,6 @@ const errorsToPass = new Set([
   'error: [polling_error] {"code":"ETELEGRAM","message":"ETELEGRAM: 409 Conflict: terminated by other getUpdates request; make sure that only one bot instance is running"}\n',
 ]);
 
-// if (!'is need update files') {
-//   [
-//     'cm',
-//     'cm/coms',
-//     'cm/comComments',
-//     'cm/comsInSchEvent',
-//     'cm/comsInSchEventHistory',
-//     'index/schedules',
-//     'index',
-//     'storages/list',
-//   ].forEach(appName => {
-//     const cb = (filePath, name) => {
-//       if (!name.endsWith('.json')) return;
-//       fs.writeFileSync(`src/back/file-stores/apps/${appName}/${name}`, '' + fs.readFileSync(filePath));
-//     };
-
-//     walkAllFiles(`src/back/apps/${appName}/+case`, cb, 1);
-//     walkAllFiles(`src/back/apps/${appName}`, cb, 1);
-//   });
-// }
-
 (async () => {
   const [backIndexFilePath] = await buildBackIndexFile();
 
