ee35823ee6bf1f234c3bcea5456149ffef5405ac
gfirf <7jesmyl@gmail.com>
2026-07-29 09:53:50 +0300

    fix(cm): event page rerenders fix

--- src/front/apps/cm/06-entities/meeting/lib/useMeetingComFaceList.tsx
+++ src/front/apps/cm/06-entities/meeting/lib/useMeetingComFaceList.tsx
@@ -2,6 +2,7 @@ import { CmComFaceList } from '$cm/entities/com-face';
 import { useCmComList } from '$cm/ext';
 import { cmIDB } from '$cm/shared/state';
 import { useLiveQuery } from 'dexie-react-hooks';
+import { useMemo } from 'react';
 import { ScheduleWidgetDayEventMi, ScheduleWidgetDayi, ScheduleWidgetWid } from 'shared/api';
 import { CmCom } from 'shared/const/cm/Com';
 import { checkIsNotNil } from 'shared/utils/checkIs';
@@ -16,7 +17,10 @@ interface Props {
 
 export const useCmMeetingComFaceList = ({ dayi, eventMi, schw, comImportantOnClick, isPutCcomFaceOff }: Props) => {
   const pack = useLiveQuery(() => schw && cmIDB.db.scheduleComws.get({ schw }), [schw]);
-  const packComws = checkIsNotNil(dayi) && checkIsNotNil(eventMi) ? (pack?.pack?.[dayi]?.[eventMi]?.s ?? []) : [];
+  const packComws = useMemo(
+    () => (checkIsNotNil(dayi) && checkIsNotNil(eventMi) ? (pack?.pack?.[dayi]?.[eventMi]?.s ?? []) : []),
+    [dayi, eventMi, pack?.pack],
+  );
   const coms = useCmComList(packComws);
 
   return {
