3eb57edf1bcf782cba915edf0663e843f3a2d22b
gfirf <7jesmyl@gmail.com>
2026-08-18 10:05:00 +0300

    fix(cm): metronome sounds as base64 src

--- src/front/widgets/metronome/lib/useMetronomePlayStateController.ts
+++ src/front/widgets/metronome/lib/useMetronomePlayStateController.ts
@@ -1,14 +1,17 @@
 import { useDebounceValue } from '#shared/lib/hooks/useDebounceValue';
 import { useAtomValue } from 'atomaric';
 import { useEffect } from 'react';
+import { iife } from 'shared/utils';
 import { takeCorrectMetronomeBpm } from 'shared/utils/cm';
-import { Loop, Sampler, getTransport } from 'tone';
+import { lazyInit } from 'shared/utils/lazyInit';
+import { getContext, getTransport, Loop, Sampler } from 'tone';
 import {
   metronomeCurrentBpmAtom,
   metronomeCurrentMeterSizeAtom,
   metronomeIsPlayAtom,
   metronomeUserMeterAccentsAtom,
 } from './atoms';
+import { lazyMetronomeSources } from './sounds';
 
 export const useMetronomePlayStateController = () => {
   const userBpm = useDebounceValue(useAtomValue(metronomeCurrentBpmAtom), 500);
@@ -22,36 +25,31 @@ export const useMetronomePlayStateController = () => {
       return;
     }
 
-    for (let beati = 0; beati < userMeterSize; beati++) {
-      const note = accents[beati] === '1' ? 'B1' : 'G1';
-      const diff = 60 / takeCorrectMetronomeBpm(userBpm);
-      const loopDuration = diff * userMeterSize;
-      const beatDiffTime = diff * beati;
+    iife(async () => {
+      const sampler = await lazySampler();
 
-      loops.push(
-        new Loop(time => {
-          sampler.triggerAttackRelease(note, '8n', time + beatDiffTime);
-        }, loopDuration),
-      );
-    }
+      for (let beati = 0; beati < userMeterSize; beati++) {
+        const note = accents[beati] === '1' ? 'B1' : 'G1';
+        const diff = 60 / takeCorrectMetronomeBpm(userBpm);
+        const loopDuration = diff * userMeterSize;
+        const beatDiffTime = diff * beati;
 
-    loops.forEach(loop => loop.start(0));
-    Transport.start();
+        loops.push(
+          new Loop(time => {
+            sampler.triggerAttackRelease(note, '8n', time + beatDiffTime);
+          }, loopDuration),
+        );
+      }
+
+      loops.forEach(loop => loop.start(0));
+      Transport.start();
+    });
 
     return stopAll;
   }, [accents, isPlay, userBpm, userMeterSize]);
 };
 
 //
-
-const sampler = new Sampler({
-  release: 1,
-  baseUrl: '/sounds/',
-  urls: {
-    A1: 'metronome1.mp3',
-    A2: 'metronome2.mp3',
-  },
-}).toDestination();
 
 const loops: Loop[] = [];
 const Transport = getTransport();
@@ -63,3 +61,35 @@ const stopAll = () => {
 
   loops.length = 0;
 };
+
+const lazySampler = lazyInit(async () => {
+  let A1;
+  let A2;
+
+  const [sound1, sound2] = lazyMetronomeSources();
+
+  const base64ToArrayBuffer = (base64DataUrl: string) => {
+    const base64 = base64DataUrl.split(',')[1];
+    const binaryString = globalThis.atob(base64);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+
+    return bytes.buffer;
+  };
+
+  try {
+    const context = getContext();
+
+    A1 = await context.decodeAudioData(base64ToArrayBuffer(sound1));
+    A2 = await context.decodeAudioData(base64ToArrayBuffer(sound2));
+  } catch {
+    A1 = sound1;
+    A2 = sound2;
+  }
+
+  return new Sampler({ urls: { A1, A2 }, release: 1 }).toDestination();
+});
