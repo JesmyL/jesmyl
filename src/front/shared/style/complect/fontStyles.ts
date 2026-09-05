34fe6d78d954e96cc4ee85f9994b382e45673fda
gfirf <7jesmyl@gmail.com>
2026-09-05 10:57:30 +0300

    fix(): font

--- src/front/shared/style/complect/fontStyles.ts
+++ src/front/shared/style/complect/fontStyles.ts
@@ -2,22 +2,22 @@ import { css } from '@emotion/react';
 
 export const fontStyles = css`
   @font-face {
-    src: url(./resource/avenir-next.ttf);
+    src: url(/resource/avenir-next.ttf);
     font-family: main;
   }
 
   @font-face {
     font-family: calypso;
-    src: url(./resource/Calypso-Regular.ttf);
+    src: url(/resource/Calypso-Regular.ttf);
   }
 
   @font-face {
     font-family: candara;
-    src: url(./resource/candara.ttf);
+    src: url(/resource/candara.ttf);
   }
 
   @font-face {
     font-family: cormorant;
-    src: url(./resource/CormorantInfant-Regular.ttf);
+    src: url(/resource/CormorantInfant-Regular.ttf);
   }
 `;
